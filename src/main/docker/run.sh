58735d5a7165671b27fd91f9b80a515a40b5adc9
Unknown <guileme13@gmail.com>
2018-09-17 22:08:00 -0300

    cap-7
    *OAuth2

--- src/main/docker/run.sh
+++ src/main/docker/run.sh
@@ -1,4 +1,4 @@
 echo "********************************************************"
 echo "Starting the Eureka Server"
 echo "********************************************************"
-java -Djava.security.egd=file:/dev/./urandom -jar /usr/local/eurekaserver/@project.build.finalName@.jar
+java  -Xmx64m -Xss256k -Djava.security.egd=file:/dev/./urandom -jar /usr/local/eurekaserver/@project.build.finalName@.jar
